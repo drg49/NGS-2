966cde723f002104139f13b63dbde537d2095abe
Danny Gavin <dan.gavin49@gmail.com>
2026-01-28 04:52:12 -0500

    Update debug manager

--- LevelFour/LevelFourDebugManager.cs
+++ LevelFour/LevelFourDebugManager.cs
@@ -6,6 +6,8 @@ public class LevelFourDebugManager : MonoBehaviour
     [SerializeField] GameObject carInteract;
     [SerializeField] GameObject carPlayer;
     [SerializeField] CarController carController;
+    [SerializeField] GameObject marcusCarPlayer;
+    [SerializeField] GameObject davidCarPlayer;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -13,6 +15,8 @@ public class LevelFourDebugManager : MonoBehaviour
         Destroy(player);
         Destroy(carInteract);
         carPlayer.SetActive(true);
+        marcusCarPlayer.SetActive(true);
+        davidCarPlayer.SetActive(true);
         carController.enabled = true;
     }
 }
