7b808924f9c12d9f8571b74d48a71049db21a3d5
Danny Gavin <dan.gavin49@gmail.com>
2026-02-03 05:52:04 -0500

    Organize fade panel script

--- LevelFive/LevelFiveFadePanel.cs
+++ LevelFive/LevelFiveFadePanel.cs
@@ -2,19 +2,42 @@ using UnityEngine;
 
 public class LevelFiveFadePanel : MonoBehaviour
 {
+    // ===== Car =====
+    [Header("Car")]
     [SerializeField] private GameObject carColliders;
     [SerializeField] private GameObject car;
     [SerializeField] private GameObject parkedCar;
+
+    // ===== Characters =====
+    [Header("Characters")]
     [SerializeField] private GameObject marcus;
+    [SerializeField] private Animator marcusAnim;
     [SerializeField] private GameObject david;
+    [SerializeField] private Animator davidAnim;
+    [SerializeField] private GameObject player;
+
+    // ===== Dialogue =====
+    [Header("Dialogue")]
     [SerializeField] private GameObject dialogueOneCam;
     [SerializeField] private GameObject dialogueOne;
+
+    // ===== Targets =====
+    [Header("Targets")]
+    [SerializeField] private Transform playerTentTarget;
+    [SerializeField] private Transform davidTentTarget;
+    [SerializeField] private Transform marcusTentTarget;
+
+    // ===== Objectives =====
+    [Header("Objectives")]
+    [SerializeField] private GameObject logObjective;
+
+    // ===== Misc =====
+    [Header("Misc")]
     [SerializeField] private GameObject marcusTent;
     [SerializeField] private GameObject davidTent;
     [SerializeField] private GameObject playerTent;
-    [SerializeField] private GameObject player;
-    [SerializeField] private Transform playerTentTarget;
-    [SerializeField] private GameObject logObjective;
+    [SerializeField] private GameObject marcusBag;
+    [SerializeField] private GameObject davidBag;
 
     public void EnterCampsite()
     {
@@ -39,6 +62,20 @@ public class LevelFiveFadePanel : MonoBehaviour
         playerTent.SetActive(true);
         // Make sure player is not inside tent after activation
         player.transform.position = playerTentTarget.position;
+        marcusAnim.SetTrigger("IdleAfterTentSetUp");
+        davidAnim.SetTrigger("IdleAfterTentSetUp");
+        // Position and rotate NPCs
+        david.transform.SetPositionAndRotation(
+            davidTentTarget.position,
+            davidTentTarget.rotation
+        );
+        marcus.transform.SetPositionAndRotation(
+            marcusTentTarget.position,
+            marcusTentTarget.rotation
+        );
+        // Destroy the items in their hands
+        Destroy(marcusBag);
+        Destroy(davidBag);
     }
 
     public void ShowLogObjective()
